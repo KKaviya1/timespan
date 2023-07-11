8c3c740a31d6e92b04160707ad5509911d2b735f
KKaviya1 <kaviyagandhi136@gmail.com>
2023-07-11 12:46:57 +0530

    my commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timespan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime matchDate = new DateTime(day: 12, month: 8, year: 2023);
+            DateTime currentDate=DateTime.Now;
+            TimeSpan ts=matchDate.Subtract(currentDate);    
+            Console.WriteLine("Days remain "+ts.Days);
+            Console.WriteLine("Days reamin" + ts.Hours);
+            Console.WriteLine(ts.ToString());
+            Console.ReadKey();
+
+        
+        
+        
+        }
+    }
+}
